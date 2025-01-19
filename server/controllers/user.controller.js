a3b2f54927134fc756fdb10806b278f089d50f76
Suchismita Rout <97534398+SuchismitaRout555@users.noreply.github.com>
2025-01-19 12:04:20 +0530

    added controllers

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,22 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+export const register = async (req, res) => {
+    try {
+        const { email, password, name, } = req.body;
+        if (!email || !password || !name) {
+            return res.status(400).json({ message: "All input is required", success: false });
+        }
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ message: "User already exists!", success: false });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        await User.create({ email, password:hashedPassword , name });
+        return res.status(201).json({ message: "User created successfully!", success: true });  
+    }catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Failed to register" , success: false });
+    }
+};

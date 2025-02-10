4f2ed643012af938e271f2a01613707998ec0572
Suchismita Rout <97534398+SuchismitaRout555@users.noreply.github.com>
2025-02-10 23:13:33 +0530

    added middlewares

--- client/src/pages/student/Profile.jsx
+++ client/src/pages/student/Profile.jsx
@@ -1,9 +1,25 @@
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Loader2 } from "lucide-react";
 import React from "react";
+import Course from "./Course";
 
 const Profile = () => {
+  const isLoading = false;
+  const enrolledCourses = [1];
+
+  
   return (
     <div className="my-24 max-w-4xl mx-auto px-4 ">
       <h1 className="font-bold text-2xl text-center md:text-left">Profile</h1>
@@ -16,40 +32,85 @@ const Profile = () => {
         </div>
 
         <div>
-            <div className="mb-2 ">
-                <h1 className="font-semibold text-gray-900 dark:text-gray-100 ">
-                    Name:
-                    <span className="font-normal text-gray-700 dark:text-gray-300 ml-2"> Suchismita Rout</span>
-                </h1>
-            </div>
-            <div className="mb-2 ">
-                <h1 className="font-semibold text-gray-900 dark:text-gray-100 ">
-                    Email:
-                    <span className="font-normal text-gray-700 dark:text-gray-300 ml-2"> suchi@gmail.comn</span>
-                </h1>
-            </div>
-            <div className="mb-2 ">
-                <h1 className="font-semibold text-gray-900 dark:text-gray-100 ">
-                    Role:
-                    <span className="font-normal text-gray-700 dark:text-gray-300 ml-2"> Instructor</span>
-                </h1>
-            </div>
-            <Dialog>
-                <DialogTrigger asChild>
-                <Button size="sm"className="mt-2">Edit Profile</Button>
-
-                </DialogTrigger>
-                <DialogContent>
-                    <DialogHeader>
-                        <DialogTitle>
-                            Edit Profile
-                        </DialogTitle>
-                        <DialogDescription>
-                            Make changes to your profile here. Click save when you're done.
-                        </DialogDescription>
-                    </DialogHeader>
-                </DialogContent>
-            </Dialog>
+          <div className="mb-2 ">
+            <h1 className="font-semibold text-gray-900 dark:text-gray-100 ">
+              Name:
+              <span className="font-normal text-gray-700 dark:text-gray-300 ml-2">
+                {" "}
+                Suchismita Rout
+              </span>
+            </h1>
+          </div>
+          <div className="mb-2 ">
+            <h1 className="font-semibold text-gray-900 dark:text-gray-100 ">
+              Email:
+              <span className="font-normal text-gray-700 dark:text-gray-300 ml-2">
+                {" "}
+                suchi@gmail.comn
+              </span>
+            </h1>
+          </div>
+          <div className="mb-2 ">
+            <h1 className="font-semibold text-gray-900 dark:text-gray-100 ">
+              Role:
+              <span className="font-normal text-gray-700 dark:text-gray-300 ml-2">
+                {" "}
+                Instructor
+              </span>
+            </h1>
+          </div>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button size="sm" className="mt-2">
+                Edit Profile
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Edit Profile</DialogTitle>
+                <DialogDescription>
+                  Make changes to your profile here. Click save when you're
+                  done.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label>Name</Label>
+                  <Input
+                    type="text"
+                    placeholder="Name"
+                    className="col-span-3"
+                  />
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label>Profile Photo</Label>
+                  <Input type="file" accept="image/*" className="col-span-3" />
+                </div>
+              </div>
+              <DialogFooter>
+                <Button disabled={isLoading}>
+                  {isLoading ? (
+                    <>
+                      <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please
+                      Wait
+                    </>
+                  ) : (
+                    "Save Changes"
+                  )}
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </div>
+      </div>
+      <div>
+        <h1 className="font-medium text-lg"> Courses you're enrolled in</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 my-5 ">
+          {enrolledCourses.length === 0 ? (
+            <h1>You haven't enrolled yet </h1>
+          ) : (
+            enrolledCourses.map((course, index) => <Course key={index} />)
+          )}
         </div>
       </div>
     </div>
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,51 +1,75 @@
-import {User} from "../models/user.model.js";
+import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { generateToken } from "../utils/generateToken.js";
 export const register = async (req, res) => {
-    try {
-        const { email, password, name, } = req.body;
-        if (!email || !password || !name) {
-            return res.status(400).json({ message: "All input is required", success: false });
-        }
-        const user = await User.findOne({ email });
-        if (user) {
-            return res.status(400).json({ message: "User already exists!", success: false });
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        await User.create({ email, password:hashedPassword , name });
-        return res.status(201).json({ message: "User created successfully!", success: true });  
-    }catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: "Failed to register" , success: false 
-
-        });
+  try {
+    const { email, password, name } = req.body;
+    if (!email || !password || !name) {
+      return res
+        .status(400)
+        .json({ message: "All input is required", success: false });
     }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res
+        .status(400)
+        .json({ message: "User already exists!", success: false });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.create({ email, password: hashedPassword, name });
+    return res
+      .status(201)
+      .json({ message: "User created successfully!", success: true });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "Failed to register", success: false });
+  }
 };
 
 export const login = async (req, res) => {
-    try{
-        const { email, password } = req.body;
-        if (!email || !password) {
-            return res.status(400).json({ message: "All input is required", success: false });
-        }
-
-        const user = await User.findOne({ email });
-        if(!user){
-            return res.status(400).json({ message: "Incorrect email or password", success: false });
-        }
-        const isPasswordMatch = await bcrypt.compare(password, user.password);
-        if(!isPasswordMatch){
-            return res.status(400).json({ message: "Incorrect email or password", success: false });
-        }
-        generateToken(user, res , `Login successfully! ${user.name}`);
-
-    }catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: "Failed to login" , success: false 
-
-        });
-
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "All input is required", success: false });
     }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "Incorrect email or password", success: false });
+    }
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res
+        .status(400)
+        .json({ message: "Incorrect email or password", success: false });
+    }
+    generateToken(user, res, `Login successfully! ${user.name}`);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Failed to login", success: false });
+  }
 };
+export const logout = async (_, res) => {
+  try {
+    return res.status( 200).cookie("token", "",{maxAge:0}).json({ message: "Logout successfully!", success: true });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Failed to logout", success: false })
+  }
+};
+export const getUserProfile = async (req, res) => {
+  try{
+    
+  }catch (error){
+    console.log(error);
+    return res.status(500).json({ message: "Failed to load user", success: false });
+  }
+}
